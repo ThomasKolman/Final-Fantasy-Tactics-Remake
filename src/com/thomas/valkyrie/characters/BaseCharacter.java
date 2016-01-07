f66df0b1ca2cd75a55ee99978b09745b70b92d9e
ThomasKolman <thomas-kolman@hotmail.com>
2016-01-06 19:28:12 -0500

    Nawab did some shit

---
+++ src/com/thomas/valkyrie/characters/BaseCharacter.java
@@ -0,0 +1,11 @@
+package com.thomas.valkyrie.characters;
+
+/**
+ * Created by Thomas on 2016-01-06.
+ */
+public class BaseCharacter
+{
+    protected int health;
+    protected int meleeAttack;
+    protected int mobility;
+}
---
+++ src/com/thomas/valkyrie/characters/Warrior.java
@@ -0,0 +1,26 @@
+package com.thomas.valkyrie.characters;
+
+/**
+ * Created by Thomas on 2016-01-06.
+ */
+public class Warrior extends BaseCharacter implements BaseActions
+{
+    public Warrior()
+    {
+        health = 100;
+        meleeAttack = 20;
+        mobility = 5;
+    }
+
+    @Override
+    public void meleeAttack()
+    {
+
+    }
+
+    @Override
+    public void move()
+    {
+
+    }
+}
